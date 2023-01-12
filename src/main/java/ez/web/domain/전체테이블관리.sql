8373ad2ca02975cdd2ea441babb19ae0c202ec5f
Varus <bobason01@gmail.com>
2023-01-12 10:14:28 +0900

    sql 수정

--- src/main/java/ez/web/domain/전체테이블관리.sql
+++ src/main/java/ez/web/domain/전체테이블관리.sql
@@ -166,9 +166,9 @@ create table tbl_suggreply(
 );
 
 create table tbl_user(
-    userId varchar(20) UNIQUE not null,
+    userId varchar(20) not null primary key,
     userPw varchar(100) not null,
-    userName varchar(20) UNIQUE not null,
+    userName varchar(20) unique not null,
     userEmail varchar(40) not null
 );
 
--- target/classes/ez/web/domain/user.sql
+++ target/classes/ez/web/domain/user.sql
@@ -1,7 +1,7 @@
 create table tbl_user(
-    userId varchar(20) UNIQUE not null,
+    userId varchar(20) not null primary key,
     userPw varchar(100) not null,
-    userName varchar(20) UNIQUE not null,
+    userName varchar(20) unique not null,
     userEmail varchar(40) not null
 );
 drop table tbl_user;
